43a58fdc9bbc0bef06429008ed23b9fc9cee644e
Kasuhira <hheflerdevelopment@gmail.com>
2020-03-26 18:48:34 -0300

    finished the basic events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_174400) do
+ActiveRecord::Schema.define(version: 2020_03_26_193257) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "date"
+    t.text "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_03_26_174400) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "events", "users"
 end
