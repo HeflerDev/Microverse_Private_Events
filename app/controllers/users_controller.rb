f139bd7a2613e2dfcd167e3745e7df39e126cb58
HeflerDev <hheflerdevelopment@gmail.com>
2020-04-03 19:37:49 -0300

    Fixed scopes issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,14 +16,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     # Separate Past and Future Events
-    @past_events = []
-    @upcoming_events = []
-    User.find(params[:id]).attended_events.each do |event|
-      if event.date < Time.now
-        @past_events << event.description
-      else
-        @upcoming_events << event.description
-      end
-    end
   end
 end
